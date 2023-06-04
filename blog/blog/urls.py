d011942202a2fa40ea89638c14fbf4546f3f863d
Oskar Ablimit <you@example.com>
2023-06-04 16:53:35 +0200

    1 push

---
+++ blog/blog/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for blog project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from blog_app import views
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.post_list, name='post_list'),
+    path('post/create/', views.post_create, name='post_create'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/update/', views.post_update, name='post_update'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+]
---
+++ blog/blog_app/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog_app/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog_app/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        post = Post.objects.create(title=title, content=content)
+        return redirect('post_detail', pk=post.pk)
+    return render(request, 'blog_app/post_create.html')
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    return render(request, 'blog_app/post_update.html', {'post': post})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog_app/post_delete.html', {'post': post})
